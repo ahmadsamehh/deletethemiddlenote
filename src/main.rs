f6aa2402161ba09200f4c0d2074e0a10f6792e4f
ahmadsamehh <ahmadsameh99@gmail.com>
2024-04-30 22:16:21 +0300

    V1

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let node: Vec<i32> = vec![1, 3, 4, 7, 1, 2, 6];
+    //you can edit your node as you want.
+
+    println!("UnEdited Node : {:?}", node);
+    println!("Edited Node : {:?}", delete_middle_note(node));
+}
+
+fn delete_middle_note(mut node_to_edit: Vec<i32>) -> Vec<i32> {
+    let nodelength = node_to_edit.len();
+    let position_to_delete = nodelength / 2;
+    println!(
+        "element to be deleted : {}",
+        node_to_edit[position_to_delete]
+    );
+    node_to_edit.remove(position_to_delete);
+    return node_to_edit;
+}
